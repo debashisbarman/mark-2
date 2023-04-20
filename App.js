4d0bc477cb77491be329964c8caf0eeeeddaca2b
debashisbarman <debashisb04@gmail.com>
2023-04-20 23:51:53 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+import { captureScreen } from 'react-native-view-shot';
+import { MD3LightTheme, Provider as PaperProvider } from 'react-native-paper';
+import Editor from './components/Editor';
+import Widget from './components/Widget';
+import { SafeAreaProvider } from 'react-native-safe-area-context/src/SafeAreaContext';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
+const App = () => {
+  const [src, setSrc] = useState('');
+
+  const [visible, setVisible] = useState(false);
+
+  const [widgetVisible, setWidgetVisible] = useState(true);
+
+  const onReset = async () => {
+    try {
+      setSrc('');
+
+      setVisible(false);
+
+      setWidgetVisible(true);
+    } catch (e) {
+      Alert.alert(e.toString());
+    }
+  };
+
+  const onScreenCapture = async () => {
+    try {
+      setWidgetVisible(false);
+
+      await new Promise(resolve => {
+        setTimeout(resolve, 100);
+      });
+
+      const uri = await captureScreen({
+        handleGLSurfaceViewOnAndroid: true,
+        quality: 1,
+      });
+
+      setSrc(uri);
+
+      setVisible(true);
+    } catch (e) {
+      Alert.alert('Something went wrong!');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <PaperProvider
+          theme={{
+            ...MD3LightTheme,
+            colors: {
+              ...MD3LightTheme.colors,
+              primary: '#6552ff',
+              background: '#ffffff',
+              surface: '#ffffff',
+              surfaceDisabled: 'rgb(229, 225, 236)',
+              tertiary: '#160647',
+            },
+            roundness: 50,
+          }}>
+          <Editor reset={onReset} src={src} visible={visible} />
+          <Widget onPress={onScreenCapture} visible={widgetVisible} />
+        </PaperProvider>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
